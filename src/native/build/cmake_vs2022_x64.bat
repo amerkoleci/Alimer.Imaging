4b8912f8caf4aa51318fc7d9d96bac1764f14d25
Amer Koleci <amerkoleci@gmail.com>
2025-01-28 17:57:53 +0100

    CI build: Update geekyeggo/delete-artifact@v5

--- src/native/build/cmake_vs2022_x64.bat
+++ src/native/build/cmake_vs2022_x64.bat
@@ -1,3 +1,3 @@
 @echo off
-cmake -S "./../" -B "vs2022_x64" -G "Visual Studio 17 2022" -A x64 -DCMAKE_INSTALL_PREFIX:String="SDK" %*
+cmake -S "./../" -B "vs2022_x64" -G "Visual Studio 17 2022" -A x64 -DCMAKE_INSTALL_PREFIX:String="win-64-sdk" %*
 echo Open vs2022_x64\Alimer.sln to build the project.
